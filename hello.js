577908c3a8adeb819a0d44063b9392a36d763ca4
Ekaterina Samuseva <samuseva.bsu@gmail.com>
2019-11-02 18:08:49 +0300

    function summ

--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
-console.log('Hello World');
+var arr = [3,2,5,6]; 
+function arraySum(array){ var sum = 0; 
+for(var i = 0; i < array.length; i++){ sum += array[i]; 
+} console.log(sum); 
+} arraySum(arr);
