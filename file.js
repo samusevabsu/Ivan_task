7efcf0598398bd65b0557f25d3579902f548696c
Ekaterina Samuseva <samuseva.bsu@gmail.com>
2019-11-18 00:33:18 +0300

    obj

--- file.js
+++ file.js
@@ -1,46 +1,45 @@
-function avers(arr){
-    let sum=arr[0];
-    let i=1;
-    do {
-    sum = sum + arr[i];
-        i++;
-    } while(i < arr.length);
-    
-    let aver= sum / arr.length;
+function avers(arr) {
+    let sum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    let aver = sum / arr.length;
     return aver;
-    };
-    avers([2,2]);
-
-
-function avers(arr){
-let sum=arr[0];
-for (let i=1; i<arr.length; i++){
-sum = sum + arr[i];
-}
-let aver= sum / arr.length;
-return aver;
 };
-avers([10,90,20,39,8]);
+avers([10, 90, 20, 39, 8]);
 
-function minarr(arr){
+function min(arr) {
     var min = arr[0];
-    for(var i=1; i<arr.length; i++){
-      if(arr[i] < min){
-        min = arr[i];   
-      }
-     }
-   return min;
-   };
-   minarr([1,2,3]);
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+};
+min([1, 2, 3]);
 
-   debugger;
-function max(arr){
- var max = arr[0];
- for(var i=1; i<arr.length; i++){
-   if(arr[i] > max){
-     max = arr[i];   
-   }
-  }
-return max;
+debugger;
+
+function max(arr) {
+    var max = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
 }
-max ([1,2,3]);
+max([1, 2, 3]);
+
+function everything(arr) {
+    let average = avers(arr);
+    let minim = min(arr);
+    let maxim = max(arr);
+    let finish = {
+        av: average,
+        mi: minim,
+        ma: maxim,
+    };
+     return finish;
+}
