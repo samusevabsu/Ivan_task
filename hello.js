ea97616649342f8f79e47272c54e4d58c34293e6
Ekaterina Samuseva <samuseva.bsu@gmail.com>
2019-11-17 23:25:45 +0300

    functions max, min, average

--- hello.js
+++ hello.js
@@ -1,5 +1,10 @@
-var arr = [3,2,5,6]; 
-function arraySum(array){ var sum = 0; 
-for(var i = 0; i < array.length; i++){ sum += array[i]; 
-} console.log(sum); 
-} arraySum(arr);
+var arr = [3, 2, 5, 6];
+
+function arraySum(array) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    console.log(sum);
+}
+arraySum(arr);
